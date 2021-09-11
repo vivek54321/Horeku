76768154e1a3f5b7eae0982ee14d00116fc07660
Vivek Sagar <45427667+vivek54321@users.noreply.github.com>
2021-09-11 13:21:31 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,26 +1,35 @@
 from flask import Flask, render_template, request
+import joblib
 
 # initialise the app
 app = Flask(__name__)
+model = joblib.load("dib_79.pkl")
 
 
 @app.route('/')
 def hello_word():
-    return render_template('home.html')
+    return render_template('diabtic.html')
+
+
 
 @app.route('/predict' , methods = ['post'])
 def predict():
-    first_name = request.form.get('fname')
-    last_name = request.form.get('lname')
-    email = request.form.get('email')
-    phone = request.form.get('phone')
+    preg = request.form.get('preg')
+    plas = request.form.get('plas')
+    pres = request.form.get('pres')
+    skin = request.form.get('skin')
+    test = request.form.get('test')
+    mass = request.form.get('mass')
+    pedi = request.form.get('pedi')
+    age = request.form.get('age')
 
-    print(first_name)
-    print(last_name)
-    print(email)
-    print(phone)
+    output = model.predict([[preg,plas,pres,skin,test,mass,pedi,age]])
+    if output[0]==1:
+        ans = 'dibatic'
+    else:
+        ans = 'not dibatic'
+    return render_template('predict.html', pred = f"person is {ans}")
 
-    return 'Form Submitted'
 
 #run the app
 if __name == '__main__':
